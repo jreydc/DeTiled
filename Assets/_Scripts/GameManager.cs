0278521caae248cc03a0bac94a072ea1021fe2cd
jreydc <jreydc@gmail.com>
2022-09-02 15:34:25 +0800

    Updated GameManager

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -11,13 +11,15 @@ public class GameManager : Singleton<GameManager>
     // Kick the game off with the first state
     void Start() => ChangeState(GameState.PREGAME);
 
-    public void ChangeState(GameState newState) {
+    public void ChangeState(GameState newState)
+    {
         OnBeforeStateChanged?.Invoke(newState);
 
         State = newState;
-        switch (newState) {
+        switch (newState)
+        {
             case GameState.PREGAME:
-                
+
                 break;
             case GameState.GAME:
                 break;
@@ -29,15 +31,16 @@ public class GameManager : Singleton<GameManager>
         }
 
         OnAfterStateChanged?.Invoke(newState);
-        
+
         Debug.Log($"New state: {newState}");
     }
 }
 
 [Serializable]
-public enum GameState {
+public enum GameState
+{
     PREGAME = 0,
     GAME = 1,
     POSTGAME = 2,
-        
+
 }
--- Assets/_Scripts/Singleton.cs
+++ Assets/_Scripts/Singleton.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Singleton<T> : MonoBehaviour where T : Singleton<T>
 {
     private static T _instance;
