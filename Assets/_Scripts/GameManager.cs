82fbe333343c8ef614fc044b19babcf55daa40bd
jreydc <53396245+jreydc@users.noreply.github.com>
2022-09-02 15:40:47 +0800

    Add files via upload

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public static event Action<GameState> OnBeforeStateChanged;
+    public static event Action<GameState> OnAfterStateChanged;
+
+    public GameState State { get; private set; }
+
+    // Kick the game off with the first state
+    void Start() => ChangeState(GameState.PREGAME);
+
+    public void ChangeState(GameState newState)
+    {
+        OnBeforeStateChanged?.Invoke(newState);
+
+        State = newState;
+        switch (newState)
+        {
+            case GameState.PREGAME:
+
+                break;
+            case GameState.GAME:
+                break;
+            case GameState.POSTGAME:
+
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+
+        OnAfterStateChanged?.Invoke(newState);
+
+        Debug.Log($"New state: {newState}");
+    }
+}
+
+[Serializable]
+public enum GameState
+{
+    PREGAME = 0,
+    GAME = 1,
+    POSTGAME = 2,
+
+}
