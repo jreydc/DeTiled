ff4e7011583a0d765e714961445b2460e82a6235
jreydc <jreydc@gmail.com>
2022-09-02 15:19:54 +0800

    Created Initial PreGame Scripts
    Initial Game Manager
    Singleton
    Load/Unload Levels

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public static event Action<GameState> OnBeforeStateChanged;
+    public static event Action<GameState> OnAfterStateChanged;
+
+    public GameState State { get; private set; }
+
+    // Kick the game off with the first state
+    void Start() => ChangeState(GameState.STARTING);
+
+    public void ChangeState(GameState newState) {
+        OnBeforeStateChanged?.Invoke(newState);
+
+        State = newState;
+        switch (newState) {
+            case GameState.PREGAME:
+                HandleStarting();
+                break;
+            case GameState.GAME:
+                HandleSpawningHeroes();
+                HandleSpawningEnemies();
+                break;
+            case GameState.POSTGAME:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+
+        OnAfterStateChanged?.Invoke(newState);
+        
+        Debug.Log($"New state: {newState}");
+    }
+}
+
+[Serializable]
+public enum GameState {
+    PREGAME = 0,
+    GAME = 1,
+    POSTGAME = 2    
+}
---
+++ Assets/_Scripts/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static T _instance;
+    public static T _Instance{ 
+        get { return _instance; }
+        private set{}
+    }
+    public virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            Debug.Log(typeof(T).ToString() + " is NULL.");
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            Debug.Log(typeof(T).ToString() + " has tried to instantiate again!");
+        }
+
+    }
+    protected virtual void OnApplicationQuit() {
+        _instance = null;
+        Destroy(gameObject);
+    }
+}
