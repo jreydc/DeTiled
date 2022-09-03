cb5daf16aaa33381022e76e6d85fcf1e5126508f
jreydc <jreydc@gmail.com>
2022-09-04 01:08:57 +0800

    Update TileSelection.cs

--- Assets/_Scripts/_GridScripts/TileSelection.cs
+++ Assets/_Scripts/_GridScripts/TileSelection.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class TileSelection : MonoBehaviour
@@ -12,7 +13,7 @@ public class TileSelection : MonoBehaviour
     #endregion
     private readonly List<Tile> _selection = new List<Tile>();
 
-    public void SelectingTiles(Tile tile){
+    public async void SelectingTiles(Tile tile){
 
         if(!_selection.Contains(tile)) _selection.Add(tile);
 
@@ -20,6 +21,19 @@ public class TileSelection : MonoBehaviour
 
         Debug.Log($"Selected tiles at ({_selection[0].x}, {_selection[0].y} and ({_selection[1].x}, {_selection[1].y}))");
 
+        await SwappingTiles(_selection[0], _selection[1]);
+
         _selection.Clear();
+    }
+
+    public async Task SwappingTiles(Tile tile1, Tile tile2){
+        var icon1 = tile1.icon;
+        var icon2 = tile2.icon;
+
+        tile1.icon.color = tile2.icon.color;
+        tile2.icon.color = icon1.color;
+        
+        await Task.Delay(1);
+        
     }
 }
